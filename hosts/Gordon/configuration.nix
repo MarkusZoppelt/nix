889e0f950d8a27803064b02d93ced8a871eb2e95
Markus Zoppelt <markus@zoppelt.net>
2024-05-27 20:19:38 +0200

    refactor: move generic NixOS config from Gordon to shared/nixos

--- hosts/Gordon/configuration.nix
+++ hosts/Gordon/configuration.nix
@@ -1,5 +1,4 @@
 { config, pkgs, ... }:
-
 {
   imports =
     [
@@ -8,74 +7,9 @@
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-
   boot.initrd.luks.devices."luks-c3f7f747-e412-4955-8f77-2d3b3ed12cbd".device = "/dev/disk/by-uuid/c3f7f747-e412-4955-8f77-2d3b3ed12cbd";
+
   networking.hostName = "Gordon";
-
-  networking.networkmanager.enable = true;
-  time.timeZone = "Europe/Berlin";
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  services.xserver.enable = true;
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-
-  programs.hyprland.enable = true;
-
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
-  };
-
-  services.printing.enable = true;
-
-  sound.enable = true;
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-  };
-
-  programs.neovim = {
-    enable = true;
-    defaultEditor = true;
-  };
-
-  users.users.mz = {
-    isNormalUser = true;
-    description = "mz";
-    extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [
-      alacritty
-    ];
-  };
-
-  programs.zsh.enable = true;
-  users.defaultUserShell = pkgs.zsh;
-
-  programs.firefox.enable = true;
-
-  nixpkgs.config.allowUnfree = true;
-
-  environment.systemPackages = with pkgs; [
-    clang
-    fzf
-    git
-    starship
-    wofi
-  ];
-
-  programs.gnupg.agent = {
-    enable = true;
-    enableSSHSupport = true;
-  };
-
-  services.openssh.enable = true;
-  services.yubikey-agent.enable = true;
-  services.pcscd.enable = true;
 
   hardware.opengl = {
     enable = true;
--- shared/nixos/common-config.nix
+++ shared/nixos/common-config.nix
@@ -1,22 +1,68 @@
 { config, pkgs, ... }:
 {
-  # Example of enabling a NixOS service
-  services.openssh.enable = true;
+  users.users.mz = {
+    isNormalUser = true;
+    description = "mz";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+      alacritty
+    ];
+  };
 
-  # Example of configuring the boot loader
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  networking.networkmanager.enable = true;
+  time.timeZone = "Europe/Berlin";
+  i18n.defaultLocale = "en_US.UTF-8";
 
-  # Example of setting up firewall
-  #networking.firewall.allowedTCPPorts = [ 22 80 443 ];
-  #networking.firewall.enable = true;
+  services.xserver.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+  programs.hyprland.enable = true;
 
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  services.printing.enable = true;
+
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  programs.tmux.enable = true;
+  programs.zsh.enable = true;
   users.defaultUserShell = pkgs.zsh;
+
+  programs.firefox.enable = true;
+
+  nixpkgs.config.allowUnfree = true;
 
   programs.neovim = {
     enable = true;
     defaultEditor = true;
   };
 
+  environment.systemPackages = with pkgs; [
+    clang
+    fzf
+    git
+    starship
+    wofi
+  ];
+
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
+
+  services.openssh.enable = true;
+  services.tailscale.enable = true;
   services.yubikey-agent.enable = true;
+  services.pcscd.enable = true;
 }
