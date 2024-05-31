076a81d953c6d906a8e726337863dff881890ea6
Markus Zoppelt <markus@zoppelt.net>
2024-05-31 14:28:11 +0200

    feat: add homebrew / cask / mas config

--- shared/darwin/common-config.nix
+++ shared/darwin/common-config.nix
@@ -4,6 +4,33 @@
     mas
   ];
 
+  homebrew = {
+    enable = true;
+    onActivation = {
+      autoUpdate = true;
+      upgrade = true;
+      cleanup = "zap";
+    };
+
+    brews = [
+      "ollama"
+      "yubikey-agent"
+    ];
+
+    casks = [
+      "alacritty"
+      "chatgpt"
+      "orbstack"
+      "raycast"
+      "wezterm"
+    ];
+
+    masApps = {
+      "Tailscale"   = 1475387142;
+      "Xcode"       = 497799835;
+    };
+  };
+
   # Enable the Nix daemon service
   services.nix-daemon.enable = true;
 }
