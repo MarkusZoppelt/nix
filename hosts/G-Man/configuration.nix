6ffdf8a2486f6f257c76c5886417340d0a32a2ef
Markus Zoppelt <markus@zoppelt.net>
2024-07-03 15:31:31 +0200

    feat: don't use common config for G-Man
    ... instead use nix flakes on a project by project basis

--- hosts/G-Man/configuration.nix
+++ hosts/G-Man/configuration.nix
@@ -1,9 +1,16 @@
 { pkgs, ... }:
 {
+  nix.settings.experimental-features = "nix-command flakes";
+  programs.direnv.enable = true;
   environment.systemPackages = with pkgs; [
     azure-cli
+    passage
     rclone
   ];
 
+  programs.zsh.enable = true;
+
+  services.nix-daemon.enable = true;
+  security.pam.enableSudoTouchIdAuth = true;
   networking.hostName = "G-Man";
 }
