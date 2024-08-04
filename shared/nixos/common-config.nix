4a23c888994ee216a1b79e2281d6559b449dad74
Markus Zoppelt <markus@zoppelt.net>
2024-08-04 11:38:45 +0200

    fix: add stdenv.cc to nixos

--- shared/nixos/common-config.nix
+++ shared/nixos/common-config.nix
@@ -96,6 +96,7 @@
     libraries = with pkgs; [
       # Add any missing dynamic libraries for unpackaged
       # programs here, NOT in environment.systemPackages.
+      pkgs.stdenv.cc.cc
     ];
   };
 }
