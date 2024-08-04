a1f528c99be5a5a93f76d9a4b88c64e8ec0c46c3
Markus Zoppelt <markus@zoppelt.net>
2024-08-05 00:29:45 +0200

    feat: add non-NixOS linux configuration

--- flake.nix
+++ flake.nix
@@ -7,9 +7,13 @@
       url = "github:LnL7/nix-darwin";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, darwin }: {
+  outputs = { self, nixpkgs, darwin, home-manager }: {
     nixosConfigurations = {
       Gordon = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
@@ -38,6 +42,17 @@
         modules =
         [
           ./hosts/G-Man/configuration.nix
+        ];
+      };
+    };
+    
+    # Adding Nix-on-Ubuntu configuration using Home Manager
+    homeConfigurations = {
+      UbuntuMachine = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.aarch64-linux;
+        modules = 
+        [
+          ./hosts/UbuntuMachine/home.nix
         ];
       };
     };
---
+++ hosts/UbuntuMachine/home.nix
@@ -0,0 +1,85 @@
+{ pkgs, ... }:
+{
+  home.username = "mz";
+  home.homeDirectory = "/home/mz";
+  home.stateVersion = "24.05";
+  programs.home-manager.enable = true;
+  nix.settings.experimental-features = "nix-command flakes";
+
+  # Specify the Nix package for the system
+  nix = {
+    package = pkgs.nix;  # Use the default Nix package
+  };
+
+  home.packages = with pkgs; [
+    age
+    age-plugin-yubikey
+    btop
+    fd
+    fzf
+    gh
+    git
+    go
+    gum
+    jq
+    lazygit
+    monaspace
+    neovim
+    nodejs
+    passage
+    qrencode
+    restic
+    ripgrep
+    rustup
+    sd
+    starship
+    tree
+    unzip
+    uutils-coreutils-noprefix
+    yt-dlp
+    yubikey-manager
+    zsh
+    zsh-autosuggestions
+    zsh-completions
+  ];
+
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+  };
+
+  programs.tmux = {
+    enable = true;
+    # keep up to date with ~/.dotfiles/.tmux.conf
+    # until I find a better way to do this...
+    extraConfig = ''
+      unbind C-b
+      set -g prefix C-a
+      bind C-a send-prefix
+      bind t split-window -f -l 15 -c "#{pane_current_path}"
+      bind T split-window -h -f -p 35 -c "#{pane_current_path}"
+      set-option -g default-terminal "screen-256color"
+      set -ag terminal-overrides ",xterm-256color:RGB"
+      set -g history-limit 9999999
+      set -g mouse on
+      setw -g mouse on
+      set -sg escape-time 0
+      # make window/pane index start with 1
+      set -g base-index 1
+      setw -g pane-base-index 1
+      # renumber windows when closing one
+      set -g renumber-windows on
+      # set bar colors
+      set -g status-style fg="#3b4261",bg="#16161e"
+      # set background color of current window to blue
+      setw -g window-status-current-style bg=default,fg="#7aa2f7"
+    '';
+  };
+  programs.direnv.enable = true;
+
+  # Set up npm so that it installs global packages in the user's home directory
+  home.sessionVariables = {
+    NPM_CONFIG_PREFIX = "$HOME/.npm";
+    PATH = "$HOME/.npm/bin:${pkgs.lib.makeSearchPath "bin" [ pkgs.nodejs ]}:$PATH";
+  };
+}
