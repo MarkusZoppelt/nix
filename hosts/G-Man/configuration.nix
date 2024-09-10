a64189002139e50f41fcfc7da230503df4e291bb
Markus Zoppelt <markus@zoppelt.net>
2024-09-10 10:23:39 +0200

    feat(g-man): use brew for LLVM / C

--- hosts/G-Man/configuration.nix
+++ hosts/G-Man/configuration.nix
@@ -1,15 +1,7 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
   environment.systemPackages = with pkgs; [
     azure-cli
-    clang_16
-    libcxx
-    cmake
-    gnumake
-    go
-    lcov
-    llvm_16
-    openjdk
   ];
 
   networking.hostName = "G-Man";
@@ -22,6 +14,14 @@
       cleanup = "zap";
     };
 
+    brews = [
+      "bazelisk"
+      "cmake"
+      "lcov"
+      "llvm@16"
+      "openjdk@17"
+    ];
+
     casks = [
       "1password"
       "1password-cli"
@@ -33,5 +33,13 @@
       "Xcode"       = 497799835;
       "Microsoft Remote Desktop" = 1295203466;
     };
+  };
+
+  environment.variables = {
+    PATH = lib.mkForce ''
+      /opt/homebrew/opt/llvm@16/bin:$HOME/.npm/bin:${pkgs.lib.makeSearchPath "bin" [ pkgs.nodejs ]}:$PATH
+    '';
+    LDFLAGS="-L/opt/homebrew/opt/llvm@16/lib";
+    CPPFLAGS="-I/opt/homebrew/opt/llvm@16/include";
   };
 }
