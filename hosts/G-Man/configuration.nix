9b99133dd3010a7ac6dbb1eb884d331faf3e6562
Markus Zoppelt <markus@zoppelt.net>
2024-09-12 21:48:57 +0200

    refactor(darwin): use orbstack only on G-Man

--- hosts/G-Man/configuration.nix
+++ hosts/G-Man/configuration.nix
@@ -25,6 +25,7 @@
     casks = [
       "1password"
       "1password-cli"
+      "orbstack"
       "slack"
       "visual-studio-code"
     ];
--- shared/darwin/common-config.nix
+++ shared/darwin/common-config.nix
@@ -19,7 +19,6 @@
 
     casks = [
       "chatgpt"
-      "orbstack"
       "raycast"
       "signal"
       "spotify"
