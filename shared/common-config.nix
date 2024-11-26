a01ff679b449b07bfc314c47c980af04d3fad4a1
Markus Zoppelt <markus@zoppelt.net>
2024-11-26 21:44:29 +0100

    fix: move darwin specific env vars to shared/darwin config

--- shared/common-config.nix
+++ shared/common-config.nix
@@ -51,14 +51,5 @@ in
     '';
   };
 
-  # Set up npm so that it installs global packages in the user's home directory
-  environment.variables = {
-    NPM_CONFIG_PREFIX = "$HOME/.npm";
-    PATH = ''
-      /opt/homebrew/opt/llvm@16/bin:$HOME/.npm/bin:${pkgs.lib.makeSearchPath "bin" [ pkgs.nodejs ]}:$PATH
-    '';
-    LDFLAGS="-L/opt/homebrew/opt/llvm@16/lib/c++ -Wl,-rpath,/opt/homebrew/opt/llvm@16/lib/c++";
-    CPPFLAGS="-I/opt/homebrew/opt/llvm@16/include";
-  };
   programs.direnv.enable = true;
 }
--- shared/darwin/common-config.nix
+++ shared/darwin/common-config.nix
@@ -5,6 +5,16 @@
     mas
   ];
 
+  # Set up npm so that it installs global packages in the user's home directory
+  environment.variables = {
+    NPM_CONFIG_PREFIX = "$HOME/.npm";
+    PATH = ''
+      /opt/homebrew/opt/llvm@16/bin:$HOME/.npm/bin:${pkgs.lib.makeSearchPath "bin" [ pkgs.nodejs ]}:$PATH
+    '';
+    LDFLAGS="-L/opt/homebrew/opt/llvm@16/lib/c++ -Wl,-rpath,/opt/homebrew/opt/llvm@16/lib/c++";
+    CPPFLAGS="-I/opt/homebrew/opt/llvm@16/include";
+  };
+
   homebrew = {
     enable = true;
     onActivation = {
