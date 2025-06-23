7779d244afb8d4d89e867ceff97b28f39c6dcc6e
Markus Zoppelt <markus@zoppelt.net>
2025-06-23 09:01:48 +0200

    feat: NVIDIA 3080 card supports the open driver
    https://nixos.wiki/wiki/Nvidia
    
    An important note to take is that the option `hardware.nvidia.open`
    should only be set to false if you have a GPU with an older architecture
    than Turing (older than the RTX 20-Series).
    
    For newer cards it is recommended by NVIDIA to use the open Drivers:
    https://download.nvidia.com/XFree86/Linux-x86_64/565.77/README/kernel_open.html

--- hosts/NixOS/configuration.nix
+++ hosts/NixOS/configuration.nix
@@ -14,13 +14,11 @@
   };
 
   ### NVIDIA / GRAPHICS ###
-  hardware.graphics.enable32Bit = true;
   services.xserver.videoDrivers = ["nvidia"];
   hardware.nvidia = {
     modesetting.enable = true;
     powerManagement.enable = false;
-    powerManagement.finegrained = false;
-    open = false;
+    open = true;
     nvidiaSettings = true;
     package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
