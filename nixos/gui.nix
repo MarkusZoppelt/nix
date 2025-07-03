355acfe8ab0b42269c9234e5ed5dcdf374ecc7b8
Markus Zoppelt <markus@zoppelt.net>
2025-07-03 16:29:11 +0200

    refactor: gui.nix structure

--- nixos/gui.nix
+++ nixos/gui.nix
@@ -1,56 +1,22 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 {
-  ### BOOTLOADER & INITRD ###
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
   systemd.services.NetworkManager-wait-online.enable = false;
 
-  ### DESKTOP ENVIRONMENT ###
-  services.xserver = {
-    enable = true;
-    xkb = {
-      layout = "us";
-      variant = "";
-    };
-  };
-  services.displayManager.gdm.enable = true;
-
-  programs.hyprland.enable = true;
-  programs.waybar.enable = true;
-  programs.hyprlock.enable = true;
-  security.pam.services.hyprlock = {};
-
-  programs.dconf.enable = true;
-  environment.sessionVariables.NIXOS_OZONE_WL = "1";
-
-  ### FONTS ###
-  fonts = import ../shared/fonts.nix { inherit pkgs; };
-
-  ### SOUND ###
-  services.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-  };
-
-  ### GRAPHICS ###
   hardware.graphics = {
     enable = true;
   };
 
-  ### USER CONFIGURATION ###
-  users.users.mz = {
-    packages = with pkgs; [
+  fonts = import ../shared/fonts.nix { inherit pkgs; };
+
+  environment = {
+    systemPackages = with pkgs; [
       _1password-cli
       _1password-gui
       chromium
       ghostty
-      gnome-settings-daemon
-      hypridle
       hyprpaper
       hyprpolkitagent
       hyprshot
@@ -60,36 +26,70 @@
       obsidian
       papers
       pavucontrol
-      pcsclite # for yubikey-stuff
+      pcsclite
       pkg-config
-      playerctl
       signal-desktop
+      tokyonight-gtk-theme
       wl-clipboard
       wofi
       xdg-desktop-portal-gtk
       xdg-desktop-portal-hyprland
     ];
+    sessionVariables.NIXOS_OZONE_WL = "1";
   };
 
-  environment.systemPackages = with pkgs; [ tokyonight-gtk-theme ];
-  programs.dconf.profiles.user.databases = [
-      {
+  programs = {
+    hyprland.enable = true;
+    waybar.enable = true;
+    hyprlock.enable = true;
+    dconf = {
+      enable = true;
+      profiles.user.databases = [{
         settings = {
           "org/gnome/desktop/interface" = {
             gtk-theme = "Tokyonight-Dark";
             color-scheme = "prefer-dark";
           };
         };
-      }
-  ];
+      }];
+    };
+  };
 
+  services = {
+    xserver = {
+      enable = true;
+      xkb = {
+        layout = "us";
+        variant = "";
+      };
+    };
 
-  services.gvfs.enable = true;
-  services.udisks2.enable = true;
-  services.samba.enable = true;
-  security.polkit.enable = true;
+    displayManager.gdm.enable = true;
+    gnome.gnome-settings-daemon.enable = true;
+    hypridle.enable = true;
+    
+    pulseaudio.enable = false;
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+    };
+    playerctld.enable = true;
 
-  ### YUBIKEY SUPPORT ###
-  services.yubikey-agent.enable = true;
-  services.pcscd.enable = true;
+    # for mounting stuff
+    gvfs.enable = true;
+    udisks2.enable = true;
+    samba.enable = true;
+
+    # YUBIKEY SUPPORT
+    yubikey-agent.enable = true;
+    pcscd.enable = true;
+  };
+
+  security = {
+    pam.services.hyprlock = {};
+    rtkit.enable = true;
+    polkit.enable = true;
+  };
 }
